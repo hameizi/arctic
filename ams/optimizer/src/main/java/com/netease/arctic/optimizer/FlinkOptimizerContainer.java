3ee0e5ce932e5d3d93b1923abb3755eb71656234
HuangFru <68625618+HuangFru@users.noreply.github.com>
2023-10-31 09:47:24 +0800

    [AMORO-2196] Fix scale out error (#2211)
    (cherry picked from commit 6f112329b674c8d36aa8a929bab48a1ddebd5dd2)

--- ams/optimizer/src/main/java/com/netease/arctic/optimizer/FlinkOptimizerContainer.java
+++ ams/optimizer/src/main/java/com/netease/arctic/optimizer/FlinkOptimizerContainer.java
@@ -276,8 +276,8 @@ public class FlinkOptimizerContainer extends AbstractResourceContainer {
             break;
           }
           LOG.info("{}", readLine);
-          if (commandReader != null) {
-            value = commandReader.apply(readLine);
+          if ((value = commandReader.apply(readLine)) != null) {
+            break;
           }
         }
         return value;
@@ -308,8 +308,7 @@ public class FlinkOptimizerContainer extends AbstractResourceContainer {
       String releaseCmd = exportCmd + " && " + releaseCommand;
       String[] cmd = {"/bin/sh", "-c", releaseCmd};
       LOG.info("Releasing flink optimizer using command: " + releaseCmd);
-      Process exec = Runtime.getRuntime().exec(cmd);
-      fetchCommandOutput(exec, null);
+      Runtime.getRuntime().exec(cmd);
     } catch (IOException e) {
       throw new UncheckedIOException("Failed to release flink optimizer.", e);
     }
